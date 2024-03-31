30b19af1ae5880adbb6c3e051defcdf771266887
Daniel C <63077752+dcrep@users.noreply.github.com>
2024-03-31 17:32:25 -0400

    Completed Tutorial

--- main.js
+++ main.js
@@ -6,3 +6,67 @@ https://youtu.be/hdI2bqOjy3c?t=4228
 
 Start at 1:10:29
  */
+
+// Single Element
+// const form = document.getElementById('my-form');
+// console.log(form);
+// console.log(document.querySelector('h1'));
+
+// Multiple Element
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByName('li'));
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+// const ul = document.querySelector('.items');
+
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// const btn = document.querySelector('.btn');
+/* btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+    //console.log(e.target);
+    //console.log('click');
+});*/
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    // console.log(nameInput.value);
+    if (nameInput.value === '' || emailInput.value === '') {
+        // alert('Please enter fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => {
+           msg.remove() 
+        }, 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        console.log('success');
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
